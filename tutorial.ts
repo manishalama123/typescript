c165cad2cdd60e24f44967307be79181930fbe40
manishalama136@gmail.com <manishalama@yojanasolutions.com.np>
2026-01-16 21:15:36 +0545

    intersection, union, type alias

--- tutorial.ts
+++ tutorial.ts
@@ -1,35 +1,25 @@
-// let id: number = 5;
-// let company: string ="Acme Corp";
-// let isPublished: boolean = true;
-// let ids: string[] = [1,2,3,3];
-// let x: any[] = ["fff"];
-
-// const concatenateValues = (a:string,b:string): string =>{
-//     return a+b;
-// }
-// console.log(concatenateValues(9,8));
-// console.log(concatenateValues("hello", "workd"));
-
-interface UserInterface{
-    id: number;
-    age?: number;
-    name: string;
-    greet(message:string): void;
-}
-
-const User: UserInterface = {
-    id:2,
-    name:"John doe",
-    greet(message){
-        console.log(message);
-        
-    }
-}
-User.greet("hello"+ User.name);
-// if(!User.age){
-//     console.log("No age of user");
+// union -> | and intersection -> &
+// type alias
+// type IDFieldType = string | number;
+// const printID = (id: IDFieldType) =>{
+//     console.log("ID" + id);
     
-// }else{
-//     console.log(User.age);
 // }
+// printID(45678);
 
+interface BusinessPartner{
+    name: string;
+    creditScore: number;
+}
+interface UserIdentity {
+    id: number;
+    email: string;
+}
+type Employee = BusinessPartner & UserIdentity;
+
+const signContract = (employee: Employee): void=>{
+    console.log("Contract signed by " + employee.name + "with email" + employee.email);
+    
+};
+
+signContract({name: "Pedro", creditScore: 800, id: 34, email:"pedro@gmail.com"})
